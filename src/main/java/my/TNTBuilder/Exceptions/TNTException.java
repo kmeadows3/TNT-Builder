ac1817dd24e62fb856879cb88a74726f561609b0
Katherine Meadows <kmeadows3@gmail.com>
2024-02-21 10:54:52 -0500

    Updated exceptions

--- src/main/java/my/TNTBuilder/Exceptions/TNTException.java
+++ src/main/java/my/TNTBuilder/Exceptions/TNTException.java
@@ -1,8 +1,16 @@
 package my.TNTBuilder.Exceptions;
 
 public class TNTException extends Exception{
+    public TNTException (){
+        super();
+    }
+
 
     public TNTException (String message){
         super(message);
+    }
+
+    public TNTException (String message, Exception cause){
+        super(message, cause);
     }
 }
